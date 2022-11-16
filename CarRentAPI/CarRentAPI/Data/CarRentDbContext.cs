a8cdbdeccea87a805698e01ede0f7965af6be404
derzyx <mkrauz33@gmail.com>
2022-11-16 19:00:22 +0100

    DbContext and initial migration without db update

---
+++ CarRentAPI/CarRentAPI/Data/CarRentDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CarRentAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRentAPI.Data
+{
+    public class CarRentDbContext: DbContext
+    {
+        public CarRentDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<RentalPlace> RentalPlaces { get; set; }
+        public DbSet<Car> Cars { get; set; }
+    }
+}
