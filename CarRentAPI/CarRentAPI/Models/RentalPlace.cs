7c7f97ff985b42945cb68598c19ef3c96c56d8d6
derzyx <mkrauz33@gmail.com>
2022-11-16 19:45:56 +0100

    Removed "AvailableCars" from RentalPlace class

--- CarRentAPI/CarRentAPI/Models/RentalPlace.cs
+++ CarRentAPI/CarRentAPI/Models/RentalPlace.cs
@@ -6,6 +6,5 @@
         public string City { get; set; }
         public List<Car>? Car { get; set; }
         public float BasePrice { get; set; }
-        public int AvailableCars => Car.Count;
     }
 }
